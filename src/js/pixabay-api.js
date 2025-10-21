a88de321eea4cb9c10a8d8205eb3e0fdb483c84c
Serhii <kallisto77@gmail.com>
2025-10-21 23:52:57 +0400

    fix main.js, pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,42 @@
 import axios from "axios";
-import { createGallery, hideLoader, messageError } from "./render-functions";
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+import { hideLoader, messageError } from "./render-functions";
+
+const form = document.querySelector('.form');
+const imageInput = form.querySelector('input[name="search-text"]');
 
 const url = "https://pixabay.com/api/";
-const gallery = document.querySelector('.gallery');
+const myAPI_KEY = "52805725-7d516d36c1804a9cebba9806b";
+const queryWord = imageInput.value;
 
-export function getImagesByQuery(option) {
 
-    axios(url, option)
+export function getImagesByQuery(queryWord) {
+    const option = {
+        params: {
+            key: myAPI_KEY,
+            q: queryWord,                // слово з пошукового поля
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+        }
+    };
+    return axios(url, option)
         .then(response => {
             if (response.data.hits.length > 0) {
-                createGallery(response.data.hits);
+                return response.data.hits;
             } else {
                 messageError();
+                return null;
             }
         })
-        .catch(error => console.log(error)
-        ) 
+        .catch(error => iziToast.show({
+        message: error.message,
+        position: `topRight`,
+        messageColor: '#fffc3aff',
+        backgroundColor: "#ec3939",
+    })) 
         .finally(() => {
             hideLoader();
         })
--- src/main.js
+++ src/main.js
@@ -1,30 +1,25 @@
 import { getImagesByQuery } from "./js/pixabay-api";
-import { clearGallery, showLoader } from "./js/render-functions";
+import { clearGallery, createGallery, hideLoader, showLoader } from "./js/render-functions";
 
-const myAPI_KEY = "52805725-7d516d36c1804a9cebba9806b";
 const form = document.querySelector('.form');
 const imageInput = form.querySelector('input[name="search-text"]');
-
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
     showLoader();
     clearGallery();
-    if (imageInput.value.length > 0) {
-        const queryWord = imageInput.value;
-    const option = {
-            params: {
-                key: myAPI_KEY,
-                q: queryWord,                // слово з пошукового поля
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-            }
-    };
-    getImagesByQuery(option);
-    } else {
-        return;
+    if (!imageInput.value.trim()) {
+        hideLoader();
+        return;    
     }
-    
+    showLoader();
+    getImagesByQuery(imageInput.value).then(images => {
+        if (images) {
+            createGallery(images);
+        }
+    });
+        
+    hideLoader();
+    event.target.reset()
 }
